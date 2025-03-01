fa32a3abe7cf980be959eed667ba47557ae6e07c
san413 <sanjayprajapati40458@gmail.com>
2025-03-01 10:24:10 +0530

    Update index.php
    added Sanjay Prajapati

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-echo "<?php echo 'Hello from new feature!'; ?>" > feature.php
+echo "<?php echo 'Hello Sanjay Prajapati from new feature!'; ?>" > feature.php
 git add .
 git commit -m "Added feature.php in new-feature branch"
 
